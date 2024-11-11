8558f00112dce3448f5f612479257e1aeb8c5854
Shahi77 <shahiinfinity02@gmail.com>
2024-11-11 18:39:35 +0530

    applying pca

---
+++ PCA/pca.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+import numpy as np
+
+file_dir = 'C:/Users/HP/Desktop/7th sem/Mtech/BioInfo/data_processing/processed.csv'
+
+df = pd.read_csv(file_dir)
+
+pca = PCA(n_components=0.99, svd_solver='full')
+
+components = []
+
+for x in range(0, len(df.columns)):
+    components.append("Component" + str(x))
+
+principal_components = pca.fit_transform(df)
+
+principal_df = pd.DataFrame(data=principal_components)
+
+np.savetxt("pca.csv", principal_df, delimiter=",")
